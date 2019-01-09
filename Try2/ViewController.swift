5ffb20655d15129808c7b73e68f1e0717db8cdb8
Вовк Ольга <vovkolga@macbook-vovk.local>
2019-01-09 23:15:51 +0200

    Initial Commit

---
+++ Try2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Try2
+//
+//  Created by Вовк Ольга' on 1/9/19.
+//  Copyright © 2019 Вовк Ольга'. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
