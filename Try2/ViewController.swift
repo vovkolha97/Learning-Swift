f0bb76ecfe558ba61d6f9432a7e438ce8a402ca2
Вовк Ольга <vovkolga@macbook-vovk.local>
2019-01-10 13:15:55 +0200

    first try with swift

--- Try2/ViewController.swift
+++ Try2/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Labe: UILabel!
+    @IBAction func Butt(_ sender: Any) {
+        var index = Int.random(in: 0..<array.count)
+        
+        while(true){
+            if(Labe.text != array[index]){
+                break
+            } else{
+              index = Int.random(in: 0..<array.count)
+            }
+            
+        }
+              Labe.text = array[index]
+    }
+    var array = ["drink more water", "watch a comedy", "call to old friend", "take a bath with foam", "take a break", "work hard", "read book", "go for a walk", "don't die", "don't forget about your friends", "remember - everything is possible", "clean your desk", "don't eat alone", "take care of your shoes"]
+    
+    
+    @IBOutlet weak var b: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
